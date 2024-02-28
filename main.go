ec6b2c9438dc4be509bd1994a84055ca8b3536ba
กฤษฎิ์ ศรีวงศ์พรธนา <krit.sr@NBTC.domain>
2024-02-28 15:58:13 +0700

    add some comments

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"net/http"
 )
 
+// home handler func
 func home(w http.ResponseWriter, r *http.Request) {
+	// protect the servemux to treat this like catch all pattern
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -13,20 +15,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from snippetbox"))
 }
 
+// snippet view handler func
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet..."))
 }
 
+// create snippet handler func
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
+	// router (servemux) mapping url pattern and corresponding handlers
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snipper/create", snippetCreate)
-
+	// establish a web server and listen for incoming requests
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
